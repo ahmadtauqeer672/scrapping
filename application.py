59af4bc9f345338dcf635228d726673b691fdfd3
ahmadtauqeer672 <tauqeerahmad672@gmail.com>
2025-03-16 11:11:30 +0530

    tau

--- application.py
+++ application.py
@@ -73,4 +73,4 @@ def review():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5003, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
